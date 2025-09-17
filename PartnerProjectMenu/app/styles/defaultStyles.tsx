4661fd3729f404f195ea50dd0974a359583126e3
Aakansh Murali <126364490+aakanshmurali@users.noreply.github.com>
2025-09-17 12:28:33 -0500

    default styles

---
+++ PartnerProjectMenu/app/styles/defaultStyles.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+import colors from "./colors";
+
+const defaultStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+        backgroundColor: colors.background
+    },
+
+    text: {
+        fontSize: 16,
+        color: colors.text.light,
+        fontWeight: 'bold'
+    },
+
+    subHeader: {
+        fontSize: 24,
+        color: colors.text.dark,
+    },
+
+    titleText: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: colors.text.dark,
+    },
+
+    card: {
+        backgroundColor: colors.primary,
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.15,
+        shadowRadius: 3.84,
+        elevation: 5,
+  },
+})
+
+export default defaultStyles;
